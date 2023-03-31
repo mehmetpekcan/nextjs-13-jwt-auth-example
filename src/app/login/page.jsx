13b920857d8c5c18bceca58c4b48ef4498c32738
mehmetpekcan <pekcanmehmett@gmail.com>
2023-03-31 18:51:32 +0300

    Clean up for starter point

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,33 +1,8 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
-
 export default function LoginPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const username = formData.get("username");
-    const password = formData.get("password");
-
-    const res = await fetch("/api/login", {
-      method: "POST",
-      body: JSON.stringify({ username, password }),
-    });
-    const { success } = await res.json();
-
-    if (success) {
-      const nextUrl = searchParams.get("next");
-      // @see: https://github.com/vercel/next.js/discussions/44149
-      router.push(nextUrl ?? "/");
-      router.refresh();
-    } else {
-      // Make your shiny error handling with a great user experience
-      alert("Login failed");
-    }
   };
 
   return (
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,20 +1,14 @@
-import { useAuth } from "@/hooks/useAuth";
 import Link from "next/link";
 
 export async function Header() {
-  const auth = await useAuth.fromServer();
-
   return (
     <header>
       <div>
         <Link href="/">Logo</Link>
       </div>
       <nav>
-        {auth ? (
-          <Link href="/panel">Panel (Protected Route)</Link>
-        ) : (
-          <Link href="/login">Login</Link>
-        )}
+        <Link href="/panel">Panel (Protected Route)</Link>
+        <Link href="/login">Login</Link>
       </nav>
     </header>
   );
